443f41f16b484bdf97cb84d8829efbd45c697f2c
ApurvaK <kulkarniapurva2003@gmail.com>
2024-04-12 11:49:51 +0530

    php

---
+++ WTL/Prac7 -PHP/dbConnection.php
@@ -0,0 +1,7 @@
+<?php
+$databaseHost = 'localhost';
+$databaseName = 'apurva';
+$databaseUsername = 'root';
+$databasePassword = '123';
+
+$mysqli = mysqli_connect($databaseHost, $databaseUsername, $databasePassword, $databaseName); 
